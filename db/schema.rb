6586e05575463e2b9c44a10dad7a7335fdc593e1
keyur-at-simform <keyur.patel@simformsolutions.com>
2022-07-07 10:35:33 +0530

    associste user to stripe customer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_03_082030) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_04_120608) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -106,6 +106,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_03_082030) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "stripe_customer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
