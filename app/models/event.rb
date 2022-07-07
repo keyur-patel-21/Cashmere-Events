fa5f670ed7ee3937e4b27a38896f82bf21eb8c68
keyur-at-simform <keyur.patel@simformsolutions.com>
2022-07-07 12:23:49 +0530

    associate event with stripe products

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,15 @@ class Event < ApplicationRecord
   has_one_attached :poster
   has_one :guide, dependent: :destroy
   accepts_nested_attributes_for :guide
+
+  def to_s
+    name
+  end
+
+  after_create do
+    event = Stripe::Product.create(name: name)
+    price = Stripe::Price.create(product: event, unit_amount: ticket_price, currency: "inr")
+    update(stripe_event_id: event.id)
+  end
 end
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_04_120608) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_07_053346) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -79,6 +79,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_04_120608) do
     t.integer "ticket_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "stripe_event_id"
   end
 
   create_table "guides", force: :cascade do |t|
