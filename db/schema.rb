6d2499f86b8fb708bb673f25fb3298dbec5063f9
Keyur Patel <keyurspatel2023@gmail.com>
2024-03-26 01:03:38 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_072222) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_26_042023) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -124,6 +124,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_072222) do
     t.string "last_name"
     t.string "stripe_customer_id"
     t.string "phone_number"
+    t.string "otp_secret"
+    t.integer "consumed_timestep"
+    t.boolean "otp_required_for_login"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
